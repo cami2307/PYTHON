844549cac3825733613ca4abc074e023922d0aed
Kattonavi <129074551+cami2307@users.noreply.github.com>
2023-11-16 09:57:37 -0500

    nuevo

--- ejerciciodifraz/disfraces.py
+++ ejerciciodifraz/disfraces.py
@@ -11,10 +11,10 @@ def verificar_ingreso():
         else:
             print("Lo siento, no cumples con los requisitos de ingreso.")
 
-# Llama a la función para verificar la edad
+# Llama a la funcion para verificar la edad
 verificar_ingreso()
 
-# Función para verificar la temática del disfraz del usuario
+# Función para verificar la tematica del disfraz del usuario
 def verificar_tematica():
     # Lista de disfraces permitidos
     temas_permitidos = ["payaso", "fantasma", "zombie"]
@@ -23,68 +23,69 @@ def verificar_tematica():
         # Solicita al usuario que ingrese su disfraz
         disfraz = input("Ingresa de qué vas disfrazado: ")
         
-        # Verifica si el disfraz ingresado está en la lista de disfraces permitidos
+        # Verifica si el disfraz ingresado esta en la lista de disfraces permitidos
         if disfraz.lower() in temas_permitidos:
             print("¡Bienvenido a la fiesta!")
             break
         else:
             print("Lo siento, no cumples con los requisitos de ingreso.")
 
-# Llama a la función para verificar el disfraz
+# Llama a la funcion para verificar el disfraz
 verificar_tematica()
 
-# Función para gestionar el número de acompañantes del usuario
+# Funcion para gestionar el numero de acompanantes del usuario
 def gestionar_acompanantes():
-    # Número máximo de acompañantes permitidos
+    # Numero maximo de acompanantes permitidos
     limite_acompanantes = 3
 
     while True:
-        # Solicita al usuario si llevará acompañantes
-        respuesta = input("¿Llevarás acompañantes? (s/n): ").lower()
+        # Solicita al usuario si llevará acompanantes
+        respuesta = input("¿Llevaras acompanantes? (s/n): ").lower()
 
-        # Si la respuesta es 's', solicita el número de acompañantes
+        # Si la respuesta es 's', solicita el numero de acompanantes
         if respuesta == 's':
-            num_acompanantes = int(input("¿Cuántos acompañantes llevarás?: "))
+            num_acompanantes = int(input("¿Cuantos acompanantes llevaras?: "))
 
-            # Verifica si el número de acompañantes es menor o igual al límite
+            # Verifica si el número de acompanantes es menor o igual al limite
             if num_acompanantes <= limite_acompanantes:
-                print(f"¡Bienvenido! Traerás a {num_acompanantes} acompañantes.")
+                print(f"¡Bienvenido! Traeras a {num_acompanantes} acompanantes.")
                 break
             else:
-                print(f"Lo siento, solo puedes traer hasta {limite_acompanantes} acompañantes.")
+                print(f"Lo siento, solo puedes traer hasta {limite_acompanantes} acompanantes.")
         elif respuesta == 'n':
-            print("¡Bienvenido! No traerás acompañantes.")
+            print("¡Bienvenido! No traerás acompanantes.")
             break
         else:
-            print("Respuesta no válida. Por favor, ingresa 's' o 'n'.")
+            print("Respuesta no valida. Por favor, ingresa 's' o 'n'.")
 
-# Llama a la función para gestionar acompañantes
+# Llama a la funcion para gestionar acompanantes
 gestionar_acompanantes()
 
-# Función para gestionar si el usuario llevará mascotas y cuántas
+# Funcion para gestionar si el usuario llevará mascotas y cuantas
 def mascota():
-    # Número máximo de mascotas permitidas
+    # Numero maximo de mascotas permitidas
     limite_mascotas = 2 
 
     while True:
-        # Solicita al usuario si llevará mascotas
-        respuesta = input("¿Llevarás mascotas? (s/n): ").lower()
+        # Solicita al usuario si llevara mascotas
+        respuesta = input("¿Llevaras mascotas? (s/n): ").lower()
 
-        # Si la respuesta es 's', solicita el número de mascotas
+        # Si la respuesta es 's', solicita el numero de mascotas
         if respuesta == 's':
-            num_mascotas = int(input("¿Cuántas mascotas llevarás?: "))
+            num_mascotas = int(input("¿Cuantas mascotas llevaras?: "))
 
-            # Verifica si el número de mascotas es menor o igual al límite
+            # Verifica si el numero de mascotas es menor o igual al límite
             if num_mascotas <= limite_mascotas:
-                print(f"¡Bienvenido! Traerás a {num_mascotas} mascotas.")
+                print(f"¡Bienvenido! Traeras a {num_mascotas} mascotas.")
                 break
             else:
                 print(f"Lo siento, solo puedes traer hasta {limite_mascotas} mascotas.")
         elif respuesta == 'n':
-            print("¡Bienvenido! No traerás mascotas.")
+            print("¡Bienvenido! No traeras mascotas.")
             break
         else:
-            print("Respuesta no válida. Por favor, ingresa 's' o 'n'.")
+            print("Respuesta no valida. Por favor, ingresa 's' o 'n'.")
 
-# Llama a la función para gestionar mascotas
+# Llama a la funcion para gestionar mascotas
 mascota()
+
